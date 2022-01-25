371f54b73daf294a7eb639a510a54ebaa83a3530
wonjjong <wonjjong.dev@gmail.com>
2022-01-25 18:39:06 +0900

    백준 1463 1로 만들기

---
+++ src/main/java/boj1463/Main.java
@@ -0,0 +1,24 @@
+package boj1463;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+
+            dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
